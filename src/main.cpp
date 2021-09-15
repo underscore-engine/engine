78a3831fd84f2f76ac2aee23f1fc1c8dbb577fce
Gregor Maclaine <thegregormaclaine@gmail.com>
2021-09-15 18:55:08 +0100

    Cleaned CMakeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Engine Initialising...\n";
+  return 0;
+}
--- src/testing.cpp
+++
@@ -1,7 +0,0 @@
-#include "engine.h"
-
-int main()
-{
-  Initialise();
-  return 0;
-}
