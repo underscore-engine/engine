8f38d5a8d7935d14dc3a329237197210f041dc79
JaneM443 <71824776+JaneM443@users.noreply.github.com>
2021-10-13 16:51:35 +0100

    Player Character added

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,19 @@ int main()
 	// in Windows at least, this must be called before creating the window
 	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
 	// Use the screenScalingFactor
-	window.create(sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor), "SFML works!");
+	window.create(sf::VideoMode(1000.0f * screenScalingFactor, 1000.0f * screenScalingFactor), "SFML works!");
 	platform.setIcon(window.getSystemHandle());
 
-	sf::CircleShape shape(window.getSize().x / 2);
-	shape.setFillColor(sf::Color::White);
-
-	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("content/sfml.png");
-	shape.setTexture(&shapeTexture);
-
 	sf::Event event;
+
+	sf::Texture texture;
+	if (!texture.loadFromFile("main_character.png"))
+	{
+		// error...
+	}
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+	sprite.setPosition(sf::Vector2f(10.f, 50.f)); // absolute position
 
 	while (window.isOpen())
 	{
@@ -30,10 +32,30 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::D)
+				{
+					sprite.move(sf::Vector2f(10.f, 0.f));
+				}
+				if (event.key.code == sf::Keyboard::A)
+				{
+					sprite.move(sf::Vector2f(-10.f, 0.f));
+				}
+				if (event.key.code == sf::Keyboard::S)
+				{
+					sprite.move(sf::Vector2f(0.f, 10.f));
+				}
+				if (event.key.code == sf::Keyboard::W)
+				{
+					sprite.move(sf::Vector2f(0.f, -10.f));
+				}
+			}
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(sprite);
 		window.display();
 	}
 
