fafbd6409e74e44afb6e74eefa62b9f7027ff755
Gregor Maclaine <thegregormaclaine@gmail.com>
2021-10-25 00:27:44 +0100

    Added new collision and physics logic to enemy movement

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,5 +1,3 @@
-// basic enemy class template
-
 #include "Enemy.hpp"
 
 Enemy::Enemy(sf::Vector2f _pos, sf::Vector2f _size) :
@@ -18,30 +16,17 @@ Enemy::Enemy(sf::Vector2f _pos, sf::Vector2f _size) :
 
 void Enemy::updatePosition(StaticSprite* platforms, sf::Vector2f player_pos)
 {
-
 	float speed = 0.1f;
 
-	// Finds closest path to player
-	// Assuming no obstacles
-	sf::Vector2f vel;
-	vel.x = player_pos.x - pos.x;
-	vel.y = 0;
+	// Gets direction to player
+	const float horiz_vel = player_pos.x > pos.x ? speed : -speed;
+	update(horiz_vel);
 
-	// Normalises path to a unit vector
-	const float mag = sqrtf(vel.x * vel.x + vel.y * vel.y);
-	vel *= speed / mag;
-
-	// Moves enemy towards player
-	pos += vel;
-
-	sf::Vector2f new_vels[2];
-	for (unsigned int i = 0; i < 1; i++)
+	for (unsigned int i = 0; i < 2; i++)
 	{
 		if (overlaps(platforms[i]))
 		{
-			correctHitboxOverlap(new_vels, platforms[i], vel, sf::Vector2f());
-			pos += new_vels[0];
-			/* (Would update velocity of second object, but its static, and so vel always = 0) */
+			correctHitboxOverlap(platforms[i]);
 		}
 	}
 
